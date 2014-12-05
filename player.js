3ed248f92918454a2ca4df5c3d09aa613384e2b2
Brian Davidson <bsdavidson@gmail.com>
2014-12-04 21:41:03 -0600

    Initial Commit of base framework.

---
+++ player.js
@@ -0,0 +1,19 @@
+Player = function(game, x, y){
+    this.game = game;
+};
+
+Player.prototype = {
+
+    preload: function () {
+
+    },
+
+    create: function () {
+
+    },
+
+    update: function(level) {
+
+    }
+
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+# LD31
+
+My game for [ludum dare #31](ludumdare.com/compo/).
+
+Nothing yet..
+
+
+## license
+Licensed under the MIT license.
