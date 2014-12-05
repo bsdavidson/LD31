8a3c4c3b2e06b14a09c6b679a98dc5012981212f
Brian Davidson <bsdavidson@gmail.com>
2014-12-04 21:41:03 -0600

    Initial Commit of base framework.

---
+++ level.js
@@ -0,0 +1,23 @@
+Level = function(game){
+
+  this.game = game;
+
+};
+
+Level.prototype = {
+
+    preload: function () {
+
+      this.game.stage.backgroundColor = '#DDDDDD';
+
+    },
+
+    create: function () {
+
+    },
+
+    update: function() {
+
+    }
+
+};
