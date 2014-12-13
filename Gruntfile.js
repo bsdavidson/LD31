816a5fe945988992371b1ebcd5194bfdb58ecd17
Brian Davidson <bsdavidson@gmail.com>
2014-12-13 15:08:14 -0800

    Added grunt http-server task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,18 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-githooks');
+  grunt.loadNpmTasks('grunt-http-server');
   grunt.loadNpmTasks('grunt-jscs');
 
   grunt.initConfig({
     githooks: {
       all: {
         'pre-commit': 'jscs jshint'
+      }
+    },
+    'http-server': {
+      dev: {
+        root: __dirname
       }
     },
     jscs: {
